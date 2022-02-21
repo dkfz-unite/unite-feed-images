845758cbcf466c4be8512c9f08ac63e457705255
vulasau <valiantsin.ulasau@gmail.com>
2022-02-21 19:04:49 +0100

    Indexing service fixes.

--- Unite.Images.Feed.Web/Services/ImageIndexingTasksService.cs
+++ Unite.Images.Feed.Web/Services/ImageIndexingTasksService.cs
@@ -1,32 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using Unite.Data.Entities.Genome.Mutations;
 using Unite.Data.Entities.Images;
 using Unite.Data.Entities.Specimens;
-using Unite.Data.Entities.Tasks;
-using Unite.Data.Entities.Tasks.Enums;
 using Unite.Data.Services;
 
 namespace Unite.Images.Feed.Web.Services
 {
-    public class ImageIndexingTasksService
+    public class ImageIndexingTasksService : IndexingTaskService<Image, int>
     {
-        private const int BUCKET_SIZE = 1000;
+        protected override int BucketSize => 1000;
 
-        private readonly DomainDbContext _dbContext;
-
-
-        public ImageIndexingTasksService(DomainDbContext dbContext)
+        public ImageIndexingTasksService(DomainDbContext dbContext) : base(dbContext)
         {
-            _dbContext = dbContext;
         }
 
-        /// <summary>
-        /// Creates only iamge indexing tasks for all existing images
-        /// </summary>
-        public void CreateTasks()
+        public override void CreateTasks()
         {
             IterateEntities<Image, int>(image => true, image => image.Id, images =>
             {
@@ -34,11 +24,7 @@ namespace Unite.Images.Feed.Web.Services
             });
         }
 
-        /// <summary>
-        /// Creates only image indexing tasks for all images with given identifiers
-        /// </summary>
-        /// <param name="imageIds">Identifiers of images</param>
-        public void CreateTasks(IEnumerable<int> imageIds)
+        public override void CreateTasks(IEnumerable<int> imageIds)
         {
             IterateEntities<Image, int>(image => imageIds.Contains(image.Id), image => image.Id, images =>
             {
@@ -46,11 +32,7 @@ namespace Unite.Images.Feed.Web.Services
             });
         }
 
-        /// <summary>
-        /// Populates all types of indexing tasks for images with given identifiers
-        /// </summary>
-        /// <param name="imageIds">Identifiers of images</param>
-        public void PopulateTasks(IEnumerable<int> imageIds)
+        public override void PopulateTasks(IEnumerable<int> imageIds)
         {
             IterateEntities<Image, int>(image => imageIds.Contains(image.Id), image => image.Id, images =>
             {
@@ -63,42 +45,43 @@ namespace Unite.Images.Feed.Web.Services
         }
 
 
-        private void CreateDonorIndexingTasks(IEnumerable<int> imageIds)
+        protected override IEnumerable<int> LoadRelatedDonors(IEnumerable<int> keys)
         {
             var donorIds = _dbContext.Set<Image>()
-                .Where(image => imageIds.Contains(image.Id))
+                .Where(image => keys.Contains(image.Id))
                 .Select(image => image.DonorId)
                 .Distinct()
                 .ToArray();
 
-            CreateTasks(TaskType.Indexing, TaskTargetType.Donor, donorIds);
+            return donorIds;
         }
 
-        private void CreateImageIndexingTasks(IEnumerable<int> imageIds)
+        protected override IEnumerable<int> LoadRelatedImages(IEnumerable<int> keys)
         {
-            CreateTasks(TaskType.Indexing, TaskTargetType.Image, imageIds);
+            return keys;
         }
 
-        private void CreateSpecimenIndexingTasks(IEnumerable<int> imageIds)
+        protected override IEnumerable<int> LoadRelatedSpecimens(IEnumerable<int> keys)
         {
             var donorIds = _dbContext.Set<Image>()
-                .Where(image => imageIds.Contains(image.Id))
+                .Where(image => keys.Contains(image.Id))
                 .Select(image => image.DonorId)
                 .Distinct()
                 .ToArray();
 
             var specimenIds = _dbContext.Set<Specimen>()
                 .Where(specimen => donorIds.Contains(specimen.DonorId))
+                .Select(specimen => specimen.Id)
                 .Distinct()
                 .ToArray();
 
-            CreateTasks(TaskType.Indexing, TaskTargetType.Specimen, specimenIds);
+            return specimenIds;
         }
 
-        private void CreateMutationIndexingTasks(IEnumerable<int> imageIds)
+        protected override IEnumerable<long> LoadRelatedMutations(IEnumerable<int> keys)
         {
             var donorIds = _dbContext.Set<Image>()
-                .Where(image => imageIds.Contains(image.Id))
+                .Where(image => keys.Contains(image.Id))
                 .Select(image => image.DonorId)
                 .Distinct()
                 .ToArray();
@@ -115,13 +98,13 @@ namespace Unite.Images.Feed.Web.Services
                 .Distinct()
                 .ToArray();
 
-            CreateTasks(TaskType.Indexing, TaskTargetType.Mutation, mutationIds);
+            return mutationIds;
         }
 
-        private void CreateGeneIndexingTasks(IEnumerable<int> imageIds)
+        protected override IEnumerable<int> LoadRelatedGenes(IEnumerable<int> keys)
         {
             var donorIds = _dbContext.Set<Image>()
-                .Where(image => imageIds.Contains(image.Id))
+                .Where(image => keys.Contains(image.Id))
                 .Select(image => image.DonorId)
                 .Distinct()
                 .ToArray();
@@ -140,59 +123,12 @@ namespace Unite.Images.Feed.Web.Services
 
             var geneIds = _dbContext.Set<AffectedTranscript>()
                 .Where(affectedTranscript => mutationIds.Contains(affectedTranscript.MutationId))
-                .Select(affectedTranscripts => affectedTranscripts.Transcript.GeneId)
+                .Where(affectedTranscript => affectedTranscript.Transcript.GeneId != null)
+                .Select(affectedTranscripts => affectedTranscripts.Transcript.GeneId.Value)
                 .Distinct()
                 .ToArray();
 
-            CreateTasks(TaskType.Indexing, TaskTargetType.Gene, geneIds);
-        }
-
-
-        protected void CreateTasks<T>(
-            TaskType type,
-            TaskTargetType targetType,
-            IEnumerable<T> keys)
-        {
-            var tasks = keys
-                .Select(key => new Task
-                {
-                    TypeId = type,
-                    TargetTypeId = targetType,
-                    Target = key.ToString(),
-                    Date = DateTime.UtcNow
-                })
-                .ToArray();
-
-            _dbContext.AddRange(tasks);
-            _dbContext.SaveChanges();
-        }
-
-
-        protected void IterateEntities<T, TKey>(
-            Expression<Func<T, bool>> condition,
-            Expression<Func<T, TKey>> selector,
-            Action<IEnumerable<TKey>> handler)
-            where T : class
-        {
-            var position = 0;
-
-            var entities = Enumerable.Empty<TKey>();
-
-            do
-            {
-                entities = _dbContext.Set<T>()
-                    .Where(condition)
-                    .Skip(position)
-                    .Take(BUCKET_SIZE)
-                    .Select(selector)
-                    .ToArray();
-
-                handler.Invoke(entities);
-
-                position += entities.Count();
-
-            }
-            while (entities.Count() == BUCKET_SIZE);
+            return geneIds;
         }
     }
 }
