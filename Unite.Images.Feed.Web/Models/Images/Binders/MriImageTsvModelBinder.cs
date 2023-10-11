8a08834fe92188c696080da4bbb4e138a8c6017f
DGitRich <daniel.rich@gmx.net>
2023-10-11 10:22:39 +0200

    Create MriImageTsvModelBinder

---
+++ Unite.Images.Feed.Web/Models/Images/Binders/MriImageTsvModelBinder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Unite.Essentials.Tsv;
+
+namespace Unite.Images.Feed.Web.Models.Images.Binders;
+
+public class MriImageTsvModelBinder : IModelBinder
+{
+    public async Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        if (bindingContext == null)
+            throw new ArgumentNullException(nameof(bindingContext));
+
+        using var reader = new StreamReader(bindingContext.HttpContext.Request.Body);
+
+        var tsv = await reader.ReadToEndAsync();
+
+        var map = new ClassMap<ImageModel>()
+            .Map(entity => entity.Id, "id")
+            ;
+
+        var model = TsvReader.Read(tsv, map).ToArray();
+
+        bindingContext.Result = ModelBindingResult.Success(model);
+    }
+}
+
